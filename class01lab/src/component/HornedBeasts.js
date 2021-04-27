1034319d66736824c2fcd687583095a16ef4eba3
SufianHamdan <sufian_hamdan@yahoo.com>
2021-04-27 03:19:05 +0300

    solved-lab02

--- class01lab/src/component/HornedBeasts.js
+++ class01lab/src/component/HornedBeasts.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
 
 class HornedBeasts extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            numOfLikes: 0
+        }
+    }    
+    increaseNumOfLikes = () =>{
+        this.setState(
+            {
+                numOfLikes: this.state.numOfLikes + 1
+            }
+        );
+        
+    }
     render(){
         return(
-            <section>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
-                <p>{this.props.description}</p>
-            </section>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.imageUrl} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>
+                    <p>{this.props.description}</p>
+                    <p>❤️ {this.state.numOfLikes}</p>
+                    </Card.Text>
+                    <Button variant="primary" onClick={this.increaseNumOfLikes}>❤️Like</Button>
+                </Card.Body>
+            </Card>
+            // <section>
+            //     <h2>{this.props.title}</h2>
+            //     <p>❤️ {this.state.numOfLikes}</p>
+            //     <img onClick={this.increaseNumOfLikes} src={this.props.imageUrl} alt={this.props.title} title={this.props.title} width={300} />
+            //     <p>{this.props.description}</p>
+            // </section>
         );
     }
 }
